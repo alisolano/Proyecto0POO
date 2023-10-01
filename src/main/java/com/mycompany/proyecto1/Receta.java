cb2f82ecf14067aa7866ec8d3165d81f01b2c7ba
aliso <aliso@DESKTOP-MIP0V9I>
2023-09-30 20:33:15 -0600

    Prueba inicial con clase receta

---
+++ src/main/java/com/mycompany/proyecto1/Receta.java
@@ -0,0 +1,39 @@
+package com.mycompany.proyecto1;
+
+
+
+public class Receta {
+    private String nombre;
+    public static int consecutivo = 0;
+    private int codigo;
+    private float duracion;
+    private float porcion;
+    private String etiqueta;
+    private Dificultad dificultad;
+
+    public Receta(String nombre, float duracion, float porcion, String etiqueta, Dificultad dificultad) {
+        consecutivo++;
+        this.nombre = nombre;
+        this.codigo = consecutivo;
+        this.duracion = duracion;
+        this.porcion = porcion;
+        this.etiqueta = etiqueta;
+        this.dificultad = dificultad;
+    }
+    
+    public void editarReceta(String nombre, float duracion, float porcion){
+        this.nombre = nombre;
+        this.duracion = duracion;
+        this.porcion = porcion;
+        
+    
+    }
+
+    public String getEtiqueta() {
+        return etiqueta;
+    }
+
+    public void setEtiqueta(String etiqueta) {
+        this.etiqueta = etiqueta;
+    }
+}
