aa830685f5ad0280714c7f3014b6779f34f5c18b
aliso <aliso@DESKTOP-MIP0V9I>
2023-10-02 12:10:19 -0600

    clase utensilio

---
+++ src/main/java/com/mycompany/proyecto1/Utensilio.java
@@ -0,0 +1,29 @@
+package com.mycompany.proyecto1;
+
+
+public class Utensilio {
+    private String nombre;
+    public static int consecutivo = 0;
+    private int codigo;
+    private boolean disponibilidad;
+
+    public Utensilio(String nombre) {
+        this.nombre = nombre;
+        consecutivo++;
+        this.codigo = consecutivo;
+        this.disponibilidad = true; 
+    }
+    
+    public void editarUtensilio(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public boolean verDisponibilidad(){
+        return disponibilidad;
+    }
+    
+    public String toString(){
+        return "Nombre: " + nombre + 
+                "Disponibilidad: " + disponibilidad;
+    }
+}
