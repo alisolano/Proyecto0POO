0c044b65dbf89d12a0ae1acdda6b940c9cbf159f
aliso <aliso@DESKTOP-MIP0V9I>
2023-10-02 12:11:52 -0600

    recetario actualizado con metodos de utensilio

--- src/main/java/com/mycompany/proyecto1/Recetario.java
+++ src/main/java/com/mycompany/proyecto1/Recetario.java
@@ -5,11 +5,20 @@ import java.util.ArrayList;
 // clase controladora
 public class Recetario {
     private ArrayList<Receta> recetas;
+    private ArrayList<Utensilio> utensilios;
 
     public Recetario() {
         this.recetas = new ArrayList<>();
+        this.utensilios = new ArrayList<>();
     }
     
+    private Utensilio obtenerUtensilio (int codigo){
+        int indice = codigo - 1;
+        if(indice >= 0 && indice < utensilios.size()){
+            return utensilios.get(indice);
+        }
+        return null;        
+    }
     private Receta obtenerReceta(int codigo){
         int indice = codigo - 1;
         if(indice >= 0 && indice < recetas.size()){
@@ -17,6 +26,32 @@ public class Recetario {
         }
         return null;
     }
+    
+    public ArrayList<String> verListaUtensilio(){
+        ArrayList<String> listaUtensilios = new ArrayList<>();
+        for (Utensilio u : utensilios){
+            String uStr = u.toString();
+            listaUtensilios.add(uStr);
+        }
+        return listaUtensilios;
+    }
+    
+    public void agregarUtensilio(String nombre){
+        Utensilio utensilioNuevo = new Utensilio(nombre);
+        utensilios.add(utensilioNuevo);
+    }
+    
+    public void eliminarUtensilio(int codigoUtensilio){
+        Utensilio utensilio = obtenerUtensilio(codigoUtensilio);
+        if (utensilio == null){
+            System.out.println("Utensilio ingresado no existe");
+        } else {
+            int utensilioRemover = codigoUtensilio - 1;
+            recetas.remove(utensilioRemover);
+        }
+    }
+    
+    
     public void agregarReceta(String nombre, float duracion, float porcion, String etiqueta, Dificultad dificultad){
         Receta recetaNueva = new Receta(nombre, duracion, porcion, etiqueta, dificultad);
         recetas.add(recetaNueva);
@@ -25,6 +60,7 @@ public class Recetario {
     public ArrayList<String> verListaRecetas(){
         ArrayList<String> listaRecetas = new ArrayList<>();
         for (Receta r : recetas){
+
             String recetaStr = r.toString();
             listaRecetas.add(recetaStr);
         }
@@ -45,7 +81,8 @@ public class Recetario {
         if (receta == null){
             System.out.println("Receta ingresada no existe");
         } else {
-            recetas.remove(codigoReceta);
+            int recetaEliminar = codigoReceta - 1;
+            recetas.remove(recetaEliminar);
         }
     }
     
